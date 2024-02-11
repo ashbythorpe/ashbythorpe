1766df5bfbab5f5ca6d20ced7113f2f07c627b65
Ashby Thorpe <ashbythorpe@gmail.com>
2024-02-11 23:34:57 +0000

    Add header anchors in blog posts

---
+++ src/app/ui/mdx.tsx
@@ -0,0 +1,26 @@
+export default function H2({ children }: { children?: React.ReactNode }) {
+  if (!children || typeof (children as string) !== "string") {
+    return <h2>{children}</h2>;
+  }
+
+  const id = getAnchor(children as string);
+
+  return (
+    <h2 id={id} className="group">
+      {children}
+      <a
+        href={`#${id}`}
+        className="pl-2 hidden group-hover:inline text-gray-500"
+      >
+        #
+      </a>
+    </h2>
+  );
+}
+
+function getAnchor(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9 ]/g, "")
+    .replace(/[ ]/g, "-");
+}
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,7 @@ import { Code } from "bright";
 import { MDXComponents } from "mdx/types";
 import CodeTheme from "./github-light-modified.json";
 import { sourceCodePro } from "@/app/ui/fonts";
+import H2 from "./app/ui/mdx";
 
 // You need this file to use MDX in server components
 // Learn more from the Next.js docs
@@ -11,5 +12,5 @@ Code.codeClassName = sourceCodePro.className;
 Code.titleClassName = sourceCodePro.className;
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return { ...components, pre: Code };
+  return { ...components, pre: Code, h2: H2 };
 }
